630ac2793cedeb69327bd4735b0fef0d426afe36
Jsorens <jsorensen@hotmail.no>
2025-10-27 10:45:16 +0100

    La til metoder for TrainRepositoryPort, og implementerte dem i TrainRepository.java. Logikken er enda ikke ferdig definert.

--- OnlyTrainsApplication/src/main/java/org/example/repository/TrainRepository.java
+++ OnlyTrainsApplication/src/main/java/org/example/repository/TrainRepository.java
@@ -1,20 +1,49 @@
 package org.example.repository;
 
-public class TrainRepository {
+import org.example.model.Train;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+public class TrainRepository implements TrainRepositoryPort {
+
+    ArrayList<Train> trains = new ArrayList<>();
 
     public TrainRepository() {
 
 
     }
 
+    // TODO lage metode for å hente tog
+    public ArrayList<Train> getTrains() {
+        // TODO Denne metoden skal hente alle tog i systemet
+        return trains;
+    }
 
     // TODO lage metode for å legge til tog
+    @Override
+    public void Addtrain(Train train) {
 
-    // TODO lage metode for å fjerne tog
-
-    // TODO lage metode for å hente tog
-
-    // TODO lage metode for å oppdatere tog
+    }
 
     // TODO Metode for å hente alle tog fra en stasjon til en tid.
+    @Override
+    public ArrayList<Train> getTrainsFromStaionFromTime(String StationName, LocalTime time) {
+        return null;
+    }
+
+    // TODO lage metode for å fjerne tog
+    @Override
+    public void removeTrain(String trainId) {
+
+    }
+    // TODO lage metode for å oppdatere tog
+    @Override
+    public void UpdateTrain(Train train) {
+
+    }
+
+
+
+
 }
--- OnlyTrainsApplication/src/main/java/org/example/repository/TrainRepositoryPort.java
+++ OnlyTrainsApplication/src/main/java/org/example/repository/TrainRepositoryPort.java
@@ -1,4 +1,20 @@
 package org.example.repository;
 
+import java.util.ArrayList;
+import org.example.model.Train;
+import java.time.LocalTime;
 public interface TrainRepositoryPort {
+
+
+    // TODO lage metode for å legge til tog
+    public void Addtrain(Train  train);
+
+    // TODO metode for å hente tog fra en stasjon fra en gitt tid.
+    public ArrayList<Train> getTrainsFromStaionFromTime(String StationName, LocalTime time);
+
+
+    public void removeTrain(String trainId);
+
+    // TODO lage metode for å oppdatere tog
+    public void UpdateTrain(Train train);
 }
