780269886385cc715efdc3ea627e8c8794fdcf62
M0rt02 <thaneshu@hiof.no>
2025-11-05 19:11:15 +0100

    Pom.xml java 21 til dev

--- OnlyTrainsApplication/src/test/java/org/OnlyTrainsApplication/test/UnitTest/TrainTest.java
+++ OnlyTrainsApplication/src/test/java/org/OnlyTrainsApplication/test/UnitTest/TrainTest.java
@@ -17,7 +17,8 @@ import java.util.function.Supplier;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 /**
- * **/
+ *
+ **/
 public class TrainTest {
     private TrainRepository trainRepository;
     // Mocker ArtistRepositoryPort slik at vi kan definere hvordan denne skal fungere i testene våre
